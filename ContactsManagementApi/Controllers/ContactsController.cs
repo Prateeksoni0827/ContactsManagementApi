6e31b96bceebc65e3dec8bcacc37c7936439e0d6
PrateekHvantage <p.soni@hvantage.com>
2024-12-03 19:59:47 -0800

    Add project files.

---
+++ ContactsManagementApi/Controllers/ContactsController.cs
@@ -0,0 +1,45 @@
+﻿using ContactsManagementApi.Models;
+using ContactsManagementApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManagementApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactsRepository _repository;
+
+        public ContactsController()
+        {
+            _repository = new ContactsRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_repository.GetAllContacts());
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Contact contact)
+        {
+            return Ok(_repository.AddContact(contact));
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Contact contact)
+        {
+            return Ok(_repository.UpdateContact(contact));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.DeleteContact(id);
+            return NoContent();
+        }
+    }
+}
---
+++ ContactsManagementApi/Services/ContactsRepository.cs
@@ -0,0 +1,48 @@
+﻿using ContactsManagementApi.Models;
+using System.Text.Json;
+
+namespace ContactsManagementApi.Services
+{
+    public class ContactsRepository
+    {
+        private readonly string _filePath = "Data/contacts.json";
+
+        public List<Contact> GetAllContacts()
+        {
+            var jsonData = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<Contact>>(jsonData) ?? new List<Contact>();
+        }
+
+        public Contact AddContact(Contact contact)
+        {
+            var contacts = GetAllContacts();
+            contact.Id = contacts.Any() ? contacts.Max(c => c.Id) + 1 : 1;
+            contacts.Add(contact);
+            File.WriteAllText(_filePath, JsonSerializer.Serialize(contacts));
+            return contact;
+        }
+
+        public Contact UpdateContact(Contact contact)
+        {
+            var contacts = GetAllContacts();
+            var existingContact = contacts.FirstOrDefault(c => c.Id == contact.Id);
+            if (existingContact == null) throw new Exception("Contact not found.");
+
+            existingContact.FirstName = contact.FirstName;
+            existingContact.LastName = contact.LastName;
+            existingContact.Email = contact.Email;
+            File.WriteAllText(_filePath, JsonSerializer.Serialize(contacts));
+            return existingContact;
+        }
+
+        public void DeleteContact(int id)
+        {
+            var contacts = GetAllContacts();
+            var contact = contacts.FirstOrDefault(c => c.Id == id);
+            if (contact == null) throw new Exception("Contact not found.");
+
+            contacts.Remove(contact);
+            File.WriteAllText(_filePath, JsonSerializer.Serialize(contacts));
+        }
+    }
+}
